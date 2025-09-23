ff581f8b8ab9070f5b7fd48cffade7422a867c33
asrarmanaru-create <asrarmanaru@gmail.com>
2025-09-23 11:30:40 +0200

    d

--- tema01/eksempler/eksempel-1.php
+++ tema01/eksempler/eksempel-1.php
@@ -1,11 +1,5 @@
 <?php    /* Eksempel 1 */
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-/*
-/*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
-/*    Programmet skriver ut en "god dag"-melding med personens navn 
-*/
+
   $fornavn=$_POST ["fornavn"];
   $etternavn=$_POST ["etternavn"];  
 	
