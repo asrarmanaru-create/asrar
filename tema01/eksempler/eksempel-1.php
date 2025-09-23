699aa58459946c947c7ad71b0a783828044acb10
asrarmanaru-create <asrarmanaru@gmail.com>
2025-09-23 11:25:03 +0200

    a

--- tema01/eksempler/eksempel-1.php
+++ tema01/eksempler/eksempel-1.php
@@ -1,4 +1,7 @@
 <?php    /* Eksempel 1 */
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 /*
 /*    Programmet mottar fra et HTML-skjema et fornavn og et etternavn ved POST-metoden
 /*    Programmet skriver ut en "god dag"-melding med personens navn 
