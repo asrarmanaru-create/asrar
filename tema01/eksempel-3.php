488c430f880532df9231fc47376831428ace9d85
asrarmanaru-create <asrarmanaru@gmail.com>
2025-09-24 20:41:23 +0200

    differansen

--- tema01/eksempel-3.php
+++ tema01/eksempel-3.php
@@ -7,11 +7,11 @@
   $tall2=$_POST ["tall2"];  /* variable gitt verdier fra feltene i HTML-skjemaet */
 	
   $sum=$tall1 + $tall2   /* beregning av summen utført  */
-  $differanse=$tall1 - $tall2;  /* beregning av differansen utført  */
+  $differansen=$tall1 - $tall2;  /* beregning av differansen utført  */
 			
   print ("Tall 1 er $tall1 <br />");
   print ("Tall 2 er $tall2 <br />");
   print ("<br />");
   print ("Summen er $sum <br />");
-  print ("Differansen er $differanse <br />");  /* resultater skrevet ut  */
+  print ("Differansen er $differansen <br />");  /* resultater skrevet ut  */
 ?>
